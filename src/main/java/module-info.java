c28df2aabaca4af042dc4d3b4c0aa2a61c7908fb
espechan1 <esperanzaperez@paucasesnovescifp.cat>
2023-05-17 22:49:29 +0200

    start

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.prova1fx {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.prova1fx to javafx.fxml;
+    exports com.example.prova1fx;
+}
